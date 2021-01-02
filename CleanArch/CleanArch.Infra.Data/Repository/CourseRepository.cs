0acc34409fad3f76eb01c4ce07a4b38f92cb3c60
khem thapa <khem1942.thapa@gmail.com>
2021-01-03 05:33:09 +0545

    Implementation of service and repository

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            _context = universityDBContext;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
